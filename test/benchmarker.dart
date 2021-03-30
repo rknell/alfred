8df1817a54dad9ecd31ba707604b324916da0426
Ryan Knell <rknell@snappyapps.com.au>
2021-03-31 09:56:24 +1000

    Fixing some dependencies and adding rudimentary benchmarking script

---
+++ test/benchmarker.dart
@@ -0,0 +1,11 @@
+import 'package:http/http.dart' as http;
+
+main() async {
+  final stopwatch = Stopwatch()..start();
+  final actions = <Future>[];
+  for (var i = 0; i < 1000; i++) {
+    actions.add(http.get(Uri.parse("http://localhost:12345/files/dummy.pdf")));
+  }
+  await Future.wait(actions);
+  print(stopwatch.elapsed);
+}
