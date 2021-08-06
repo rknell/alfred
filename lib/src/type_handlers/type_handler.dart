d01b5315c508b7060fd72ae14a3d2ea3d7602dd5
DMA <d.markey@castsoftware.com>
2021-08-06 09:21:18 +0200

    * don't wrap type handler function, just invoke

--- lib/src/type_handlers/type_handler.dart
+++ lib/src/type_handlers/type_handler.dart
@@ -1,16 +1,13 @@
 import 'dart:async';
 import 'dart:io';
 
-typedef TypeHandlerFunction<T> = FutureOr Function(HttpRequest req, HttpResponse res, T value);
-
 class TypeHandler<T> {
-  TypeHandler(TypeHandlerFunction<T> handler) : handler = _wrap(handler);
+  TypeHandler(this._handler);
 
-  TypeHandlerFunction<dynamic> handler;
+  FutureOr Function(HttpRequest, HttpResponse, T)  _handler;
+
+  FutureOr handler(HttpRequest req, HttpResponse res, dynamic item)
+      => _handler(req, res, item as T);
 
   bool shouldHandle(dynamic item) => item is T;
-}
-
-TypeHandlerFunction<dynamic> _wrap<T>(TypeHandlerFunction<T> handler) {
-  return (HttpRequest req, HttpResponse res, dynamic value) => handler(req, res, value as T);
 }
