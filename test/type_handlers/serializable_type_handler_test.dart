47d963b7a10652d89815b596c62f45450870afa9
tomwyr <tomwyr@hotmail.com>
2022-12-30 20:55:33 +0100

    Test serializable types content type

--- test/type_handlers/serializable_type_handler_test.dart
+++ test/type_handlers/serializable_type_handler_test.dart
@@ -38,6 +38,39 @@ void main() {
     expect(response3.statusCode, 500);
     expect(response3.body.contains('_NonSerializableObj'), true);
   });
+
+  test('it sets the mime type correctly for serializable types', () async {
+    app.get('/testSerializable1', (req, res) async {
+      return _SerializableObjType1();
+    });
+    app.get('/testSerializable2', (req, res) async {
+      return _SerializableObjType1();
+    });
+    app.get('/testNoSerialize', (req, res) async {
+      return _NonSerializableObj();
+    });
+
+    final response1 =
+        await http.get(Uri.parse('http://localhost:$port/testSerializable1'));
+    expect(
+      response1.headers['content-type'],
+      'application/json; charset=utf-8',
+    );
+
+    final response2 =
+        await http.get(Uri.parse('http://localhost:$port/testSerializable2'));
+    expect(
+      response2.headers['content-type'],
+      'application/json; charset=utf-8',
+    );
+
+    final response3 =
+        await http.get(Uri.parse('http://localhost:$port/testNoSerialize'));
+    expect(
+      response3.headers['content-type'],
+      isNot('application/json; charset=utf-8'),
+    );
+  });
 }
 
 class _SerializableObjType1 {
